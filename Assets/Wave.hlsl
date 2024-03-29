529b31d934bcf9674bb36afd1a3fdfdb8a93c8a5
Keijiro Takahashi <keijiro@gmail.com>
2024-03-29 23:06:31 +0900

    Noise

--- Assets/Wave.hlsl
+++ Assets/Wave.hlsl
@@ -1,4 +1,11 @@
-void WavePattern_float(float Pos, float2 uv, float Seed, out float outAlpha)
+#include "Packages/jp.keijiro.noiseshader/Shader/SimplexNoise2D.hlsl"
+
+void WavePattern_float(float2 Pos, float Time, out float outAlpha)
 {
-    outAlpha = uv.y * ConstructFloat(JenkinsHash(float2(Pos/4, Seed + 1234 * floor(_Time.y * 5))));
+    float t = Time * 0.6 + Pos.x * 0.04;
+    float n1 = SimplexNoise(float2(Pos.y * 10, t + 100));
+    float n2 = SimplexNoise(float2(Pos.y * 20, t + 200)) * 0.5;
+    float n3 = SimplexNoise(float2(Pos.y * 40, t + 300)) * 0.25;
+    float r = ConstructFloat(JenkinsHash(float2(Pos.x * 256, Time * 100)));
+    outAlpha = smoothstep(1, 1.1, n1 + n2 + n3 + r * 0.1);
 }
