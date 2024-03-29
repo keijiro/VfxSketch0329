e9463d161d5a7296192e0e9ae3797f1256c42c47
Keijiro Takahashi <keijiro@gmail.com>
2024-03-29 23:38:07 +0900

    multiplex

--- Assets/Wave.hlsl
+++ Assets/Wave.hlsl
@@ -1,11 +1,16 @@
 #include "Packages/jp.keijiro.noiseshader/Shader/SimplexNoise2D.hlsl"
 
-void WavePattern_float(float2 Pos, float Time, out float outAlpha)
+void WavePattern_float
+  (float2 Pos, float Freq, float Thresh,
+   float Time, float Seed, float3 Color,
+   out float3 outColor)
 {
-    float t = Time * 0.6 + Pos.x * 0.04;
-    float n1 = SimplexNoise(float2(Pos.y * 10, t + 100));
-    float n2 = SimplexNoise(float2(Pos.y * 20, t + 200)) * 0.5;
-    float n3 = SimplexNoise(float2(Pos.y * 40, t + 300)) * 0.25;
+    float y = Pos.y * Freq * 10;
+    float t = Time * 0.6 + Pos.x * 0.04 + Seed;
+    float n1 = SimplexNoise(float2(y * 1, t + 100));
+    float n2 = SimplexNoise(float2(y * 2, t + 200));
+    float n3 = SimplexNoise(float2(y * 4, t + 300));
     float r = ConstructFloat(JenkinsHash(float2(Pos.x * 256, Time * 100)));
-    outAlpha = smoothstep(1, 1.1, n1 + n2 + n3 + r * 0.1);
+    float a = n1 + n2 * 0.5 + n3 * 0.25 + r * 0.1;
+    outColor = Color * smoothstep(Thresh, Thresh + 0.01, a);
 }
